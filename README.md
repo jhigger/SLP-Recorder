1339a909d932768a70e0fa40f769f7ac6a51accb
jhigger <njohnjhigger@gmail.com>
2021-11-22 17:54:56 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Record daily slp to firestore using axie infinity api and cron job
---
+++ index.js
@@ -0,0 +1,79 @@
+const {initializeApp} = require('firebase/app');
+const {
+	getFirestore,
+	collection,
+	addDoc,
+	getDocs,
+	serverTimestamp
+} = require('firebase/firestore');
+const fetch = require('node-fetch');
+const cron = require('node-cron');
+require('dotenv').config();
+
+const firebaseConfig = {
+	apiKey: process.env.apiKey,
+	authDomain: process.env.authDomain,
+	projectId: process.env.projectId,
+	storageBucket: process.env.storageBucket,
+	messagingSenderId: process.env.messagingSenderId,
+	appId: process.env.appId
+};
+
+initializeApp(firebaseConfig);
+const db = getFirestore();
+
+const getSLP = async (ronin) => {
+	const address = ronin.replace('ronin:', '0x');
+	const response = await fetch(
+		`https://game-api.axie.technology/slp/${address}`
+	);
+	const data = await response.json();
+	const slp = data[0].total;
+	return slp;
+};
+
+// const user = {
+// 	ronin: 'ronin:988a8c5d5040ea80a9dee65796bb5351b0eed719',
+// 	name: 'jhigger'
+// };
+
+// addDoc(collection(db, 'users'), user).then((doc) => {
+// 	const id = doc.id;
+// });
+
+const addRecordForAllUsers = () => {
+	getDocs(collection(db, 'users'))
+		.then((snapshot) => {
+			snapshot.docs.forEach((doc) => {
+				console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+				const id = doc.id;
+				const ronin = doc.data().ronin;
+
+				getSLP(ronin).then((slp) => {
+					const record = {
+						slp: slp,
+						timestamp: serverTimestamp()
+					};
+
+					addDoc(collection(db, 'users', id, 'records'), record).then((doc) => {
+						console.log(`${doc.id} => slp:${slp}`);
+					});
+				});
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
+
+console.log('Starting CRON Job');
+cron.schedule(
+	'59 7 * * *',
+	() => {
+		console.log('Running addRecordForAllUsers every 7:59 AM');
+		addRecordForAllUsers();
+	},
+	{
+		timezone: 'Asia/Manila'
+	}
+);
